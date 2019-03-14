89b97d98468b005458bd9ea15c4288db444b2d49
Alaa Ayyash <aja5063@gmail.com>
2019-03-14 16:40:04 -0400

    test_hello to test_app and def hello to __init__.py

--- flask_boilerplate/__init__.py
+++ flask_boilerplate/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+from flask import Flask, request
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
@@ -11,9 +13,14 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
-        
+
     @app.route('/')
     def index():
-        return 'Hello World!'
+        return 'This is a flask-boilerplate project, not to be used in production.'
+
+    @app.route('/hello')
+    def hello():
+        name = request.args.get('name', 'World')
+        return f'Hello {name}!'
 
     return app
